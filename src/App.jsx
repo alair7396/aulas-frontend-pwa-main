7d57f77d8bc257fd92aff42b8e2d08955e9465b6
alair7396 <alair_luiz@estudante.senai.br>
2024-09-25 22:23:51 -0300

    Separacao_componentes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Cabecalho from './comum/Componentes/Cabecalho/cabecalho';
+import Principal from './comum/Componentes/Principal/Principal';
+import Rodape from './comum/Componentes/Rodape/rodape';
+
+function App() {
+  return (
+    <>
+      <Cabecalho />
+      <Principal/>
+      <Rodape/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/comum/Componentes/Rodape/rodape.jsx
@@ -0,0 +1,7 @@
+import './Rodape.css';
+
+function Rodape(){
+    return <footer className='Rodape_root'>Rodape</footer>;
+}
+
+export default Rodape;
